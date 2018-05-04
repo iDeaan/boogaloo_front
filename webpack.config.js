4f8706a123f7d144bdd643434f69989f296a2bd9
volodia.g <volodia.gp@gmail.com>
2018-05-04 18:50:08 +0300

    webpack stylelint & overlay errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const webpack = require('webpack');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
   target: "web",
   cache: true,
-  entry: './src/main.js',
+  entry: [
+    './src/main.js',
+    'webpack-hot-middleware/client?path=http://localhost:3030/__webpack_hmr'
+  ],
   output: {
     filename: 'build.js'
   },
@@ -16,18 +20,23 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: [{
-          loader: "style-loader" // creates style nodes from JS strings
-        }, {
-          loader: "css-loader" // translates CSS into CommonJS
-        }, {
-          loader: "sass-loader" // compiles Sass to CSS
-        }]
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
       }
     ]
   },
   plugins: [
     new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new StyleLintPlugin()
   ]
 };
